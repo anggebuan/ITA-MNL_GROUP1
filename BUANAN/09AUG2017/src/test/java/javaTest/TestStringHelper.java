1544fdc973afe3e38d838137043acd370df6e0d7
BUANAN <BUANAN@ZHA-ITA102-W7.corp.oocl.com>
2017-08-09 14:57:11 +0800

    merge conflict testing

---
+++ BUANAN/09AUG2017/src/test/java/javaTest/TestStringHelper.java
@@ -0,0 +1,14 @@
+package javaTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestStringHelper {
+
+	@Test
+	public void testTruncateAInFirst2Position() {
+		fail("Not yet implemented");
+	}
+
+}
