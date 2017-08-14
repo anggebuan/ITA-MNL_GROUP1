66f815fbb153d7608d27dbea574db72e2fc3ff44
ESCOBJO <ESCOBJO@ZHA-ITA006-W7.corp.oocl.com>
2017-08-14 10:43:05 +0800

    as

--- 09AUG2017_ESCOBJO/src/main/java/com/oocl/escobjo/sw1/sorting.java
+++ 09AUG2017_ESCOBJO/src/main/java/com/oocl/escobjo/sw1/sorting.java
@@ -1,5 +1,8 @@
 package com.oocl.escobjo.sw1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 //ort
@@ -15,11 +18,40 @@ public class sorting {
 	}
 	
 	public static void sort(int arr[]) {
-		System.out.println("How many numbers you want to sort ?");
-		System.out.println("Enter a number: ");
-		Scanner input = new Scanner(System.in);
-		int num = arr.length;
+//		System.out.println("How many numbers you want to sort ?");
+//		System.out.println("Enter a number: ");
+//		Scanner input = new Scanner(System.in);
+//		int num = arr.length;
+//		
+//		num = input.nextInt();
 		
-		num = input.nextInt();
+		
+		  int i;
+		  BufferedReader bf = new BufferedReader(
+		  new InputStreamReader(System.in));
+		  System.out.println("Enter number:");
+		  int num = 0;
+		try {
+			num = Integer.parseInt(bf.readLine());
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		  System.out.println("Prime number: ");
+		  for (i=1; i < num; i++ ){
+		  int j;
+		  for (j=2; j<i; j++){
+		  int n = i%j;
+		  if (n==0){
+		  break;
+		  }
+		  }
+		  if(i == j){
+		  System.out.print("  "+i);
+		  }
+		  }
 	}
 }
