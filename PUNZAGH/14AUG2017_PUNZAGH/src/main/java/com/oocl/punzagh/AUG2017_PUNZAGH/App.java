86f0d5ecd793280287ee9851178b0d3ddb61092b
PUNZAGH <PUNZAGH@ZHA-ITA027-W7.corp.oocl.com>
2017-08-15 13:11:28 +0800

    UPDATE Day 14 Exercises and Assignment

---
+++ PUNZAGH/14AUG2017_PUNZAGH/src/main/java/com/oocl/punzagh/AUG2017_PUNZAGH/App.java
@@ -0,0 +1,13 @@
+package com.oocl.punzagh.AUG2017_PUNZAGH;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
