6a0043312699d6f5222b47897405d4ee9bd7e932
Jasper <Jasper@10.0.1.65>
2017-08-17 00:29:43 +0800

    update assignment # 4

--- BARCEJA2/16AUG2017_BARCEJA2/src/com/oocl/barceja2/hw2/mainOrder.java
+++ BARCEJA2/16AUG2017_BARCEJA2/src/com/oocl/barceja2/hw2/mainOrder.java
@@ -8,10 +8,8 @@ public class mainOrder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ApplicationContext context = new ClassPathXmlApplicationContext("/com/oocl/barceja2/hw2/Beans.xml");
-		ordersClass obj = (ordersClass) context.getBean("order");
-		System.out.println(obj.getItem());
-		System.out.println(obj.getAddress());
-		System.out.println(obj.getPrice());
+		classA obj = (classA) context.getBean("classA");
+		obj.displayItems();
 		
 	}
 
