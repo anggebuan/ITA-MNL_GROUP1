4ff18704e6fbc64b2e3e08232f51e0b682ac5682
TENGKH <TENGKH@ZHA-ITA024-W7.corp.oocl.com>
2017-08-16 20:07:41 +0800

    Exercises

---
+++ TENGKH/16AUG2017/16AUG2017_TENGKH/src/main/java/com/oocl/tengkh/samples/HelloSpringTest.java
@@ -0,0 +1,18 @@
+package com.oocl.tengkh.samples;
+
+public class HelloSpringTest {
+
+	private String message;
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+	public void getMessage() {
+		System.out.println(message);
+	}
+	
+	
+	
+} 
---
+++ TENGKH/16AUG2017/16AUG2017_TENGKH/src/main/java/com/oocl/tengkh/sw1/Shapes.java
@@ -0,0 +1,13 @@
+package com.oocl.tengkh.sw1;
+
+public class Shapes {
+	protected String name;
+
+	public Shapes(){}
+
+	public void draw() {
+		System.out.println("I am a " + name);
+
+	}
+
+}
