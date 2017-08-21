1d4c05b5b7bc660ed662913d2bfcfdf18a5b929f
TENGKH <TENGKH@ZHA-ITA024-W7.corp.oocl.com>
2017-08-21 18:19:32 +0800

    homework 1

---
+++ TENGKH/21AUG2017/21AUG2017_TENGKH/src/com/oocl/tengkh/resource/FileDirCounterSrc.java
@@ -0,0 +1,9 @@
+package com.oocl.tengkh.resource;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/app")
+public class FileDirCounterSrc extends Application{
+
+}
