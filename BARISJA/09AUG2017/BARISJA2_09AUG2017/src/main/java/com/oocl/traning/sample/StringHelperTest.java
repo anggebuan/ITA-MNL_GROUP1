26dcc62d8656861cbce2b5040eaf681e8f5ebb14
barisja2 <janiel.matthew.bariso@oocl.com>
2017-08-24 17:20:21 +0800

    Aug 9, 2017

---
+++ BARISJA/09AUG2017/BARISJA2_09AUG2017/src/main/java/com/oocl/traning/sample/StringHelperTest.java
@@ -0,0 +1,14 @@
+package com.oocl.traning.sample;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringHelperTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ BARISJA/09AUG2017/BARISJA2_09AUG2017/src/test/java/com/oocl/training/sample/StringHelperTest.java
@@ -0,0 +1,30 @@
+package com.oocl.training.sample;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.oocl.traning.sample.StringHelper;
+
+public class StringHelperTest {
+	
+	StringHelper helper;
+
+	@Before
+	public void before() {
+		helper = new StringHelper();
+	}
+	
+	@Test
+	public void testTruncateAInFirst2Position() {
+		
+	}
+
+	@Test
+	public void testAreFirstAndLastTwiCharactersTheSame() {
+//		fail("Not yet implemented");
+	}
+
+	
+	
+	
+}
